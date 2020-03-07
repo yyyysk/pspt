d7986ac71fb546d934fc1514731fa11ed50bea46
ysk-94 <00ysk.tech@gmail.com>
2020-03-07 11:53:38 +0900

    init

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+require('dotenv').config();
+const PORT = process.env.PORT || 5000;
+const flash = require('connect-flsh');
+const passport = require('passport');
+const request = require('request');
+const session = require('express-session');
+const app = express();
+const bodyParser = require('body-parser');
+const path = require('path');
+
+app.use(require('cookie-parser')());
+app.use(require('body-parser').urlencoded({ extended: true }));
+const expressSession = require('express-session');
+app.use(expressSession({secret: 'mySecretKey'}));
+app.use(passport.initialize());
+
+app.use('/public', express.static(__dirname + '/public'));
+app.use(flash());
+app.use(session({ secret: 'keyboard cat' }));
+app.use(bodyParser());
+app.set('view engine', 'pug');
+app.set('view options', { layout: false });
+
+require('./lib/routes.js')(app);
+
